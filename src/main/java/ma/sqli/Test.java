a9a27191d1456a527fcc1afb8a47c4e63f0c5061
Salmane Zerradi <szerradi@SMB.SQLI.COM>
2022-04-19 13:37:36 +0000

    Comments

--- src/main/java/ma/sqli/Test.java
+++ src/main/java/ma/sqli/Test.java
@@ -1,5 +1,9 @@
+/*
+*comment
+ */
 package ma.sqli;
 public class Test{
+    // main function : --------------------------------
     public static void main(String [] args){
         System.out.println("Hello!");
     }
